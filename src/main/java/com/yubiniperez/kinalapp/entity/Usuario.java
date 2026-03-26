fdfc2d4647fef657a9182427372271907c8a5fc3
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-03-26 00:46:18 -0600

    feat: add entity the user to Usuario.java

---
+++ src/main/java/com/yubiniperez/kinalapp/entity/Usuario.java
@@ -0,0 +1,80 @@
+package com.yubiniperez.kinalapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long codigoUsuario;
+    @Column (nullable = false)
+    private String username;
+    @Column (nullable = false)
+    private String password;
+    @Column (nullable = false)
+    private String email;
+    @Column (nullable = false)
+    private String rol;
+    @Column (nullable = false)
+    private int estado;
+
+    public Usuario (){}
+
+    public Usuario(Long codigoUsuario, String username, String password, String email, String rol, int estado) {
+        this.codigoUsuario = codigoUsuario;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    public Long getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(Long codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+}
