d4e90af7d6ccee5b21abfca2ed3f87d70545e844
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-03-27 10:38:27 -0600

    feat: add view the entity sale in IVentaService.java

---
+++ src/main/java/com/yubiniperez/kinalapp/service/IVentaService.java
@@ -0,0 +1,23 @@
+package com.yubiniperez.kinalapp.service;
+
+import com.yubiniperez.kinalapp.entity.Producto;
+import com.yubiniperez.kinalapp.entity.Venta;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IVentaService {
+    List<Venta> listarVentas();
+
+    Optional<Venta> buscarPorId(Long id);
+
+    Venta crear(Venta venta);
+
+    Venta actualizar(Long id, Venta venta);
+
+    void eliminar(Long id);
+
+    List<Producto> listarActivos();
+
+    boolean existePorId(Long id);
+}
