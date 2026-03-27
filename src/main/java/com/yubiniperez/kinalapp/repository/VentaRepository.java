7390af844d74cd9008a91f6f6b97b4457bc4fbcf
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-03-27 08:45:29 -0600

    feat: add repository the entity sale in VentaRepository.java

---
+++ src/main/java/com/yubiniperez/kinalapp/repository/VentaRepository.java
@@ -0,0 +1,7 @@
+package com.yubiniperez.kinalapp.repository;
+
+import com.yubiniperez.kinalapp.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
