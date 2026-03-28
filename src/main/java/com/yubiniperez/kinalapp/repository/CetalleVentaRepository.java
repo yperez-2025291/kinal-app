9dfa163bdf3440ca8fd4c082ee4b40d9733a2910
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-03-27 21:45:52 -0600

    feat: add repository the details sale in DetalleVentaRepository.java

---
+++ src/main/java/com/yubiniperez/kinalapp/repository/CetalleVentaRepository.java
@@ -0,0 +1,7 @@
+package com.yubiniperez.kinalapp.repository;
+
+import com.yubiniperez.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+}
