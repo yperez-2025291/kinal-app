68b34295e1c1bea13dd06ac190508c77152e1a85
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-03-27 21:47:38 -0600

    refactor: rename the repository DetalleVentaRepository.java

--- src/main/java/com/yubiniperez/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/yubiniperez/kinalapp/repository/DetalleVentaRepository.java
@@ -3,5 +3,5 @@ package com.yubiniperez.kinalapp.repository;
 import com.yubiniperez.kinalapp.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
 }
