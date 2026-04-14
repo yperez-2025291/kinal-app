811ef1649460003b9b5577687d743b55da903e87
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-04-14 16:31:25 -0600

    feat: configure HomeViewController with model attributes

--- src/main/java/com/yubiniperez/kinalapp/controller/HomeViewController.java
+++ src/main/java/com/yubiniperez/kinalapp/controller/HomeViewController.java
@@ -1,13 +1,22 @@
 package com.yubiniperez.kinalapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeViewController {
 
     @GetMapping ("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("titulo", "Bienvenido a Kinal App");
+        model.addAttribute("mensaje", "Sistema de Gestión Integral");
+
+        model.addAttribute("totalClientes", 120);
+        model.addAttribute("totalProductos", 450);
+        model.addAttribute("totalProveedores", 15);
+        model.addAttribute("totalVentas", 890);
+        model.addAttribute("totalEmpleados", 25);
         return "index";
     }
 }
