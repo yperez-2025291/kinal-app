370fbb6d389ed809b0803c6e12bbe121adb4bcbb
yperez-2025291 <yperez-2025291@kinal.edu.gt>
2026-04-17 21:24:12 -0600

    fix: delete archives the branch main

--- src/main/java/com/yubiniperez/kinalapp/repository/DetalleVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yubiniperez.kinalapp.repository;
-
-import com.yubiniperez.kinalapp.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
